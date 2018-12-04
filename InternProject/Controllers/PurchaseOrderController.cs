d21a1c8dc4098073374367f9af80cb91b8aad9e3
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-05 01:03:56 +0700

    Add Create Model for POManager, add POController, add Create method in POControl, add View for Create method, add Unity.MVC for implementing the Repository and Interface

--- InternProject/Controllers/PurchaseOrderController.cs
+++ InternProject/Controllers/PurchaseOrderController.cs
@@ -7,29 +7,102 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using InternProject.ViewModels.PurchaseOrderManager;
+using BLL_Layer.BLL.Interface.PurchaseOrderTool;
+using DAL_Layer.DAL.Models.OrderTrack;
 
 namespace InternProject.Controllers
 {
     public class PurchaseOrderController : Controller
     {
-        /*public ActionResult Create()
+        private IPurchaseOrderRepository PurchaseOrder;
+
+        public PurchaseOrderController() { }
+
+        public PurchaseOrderController(IPurchaseOrderRepository purchaseOrder)
         {
-            return View();
+            this.PurchaseOrder = purchaseOrder;
+        }
+
+        public ActionResult Create()
+        {
+            AddModel defaultModel = new AddModel();
+            defaultModel.Seasons = GetSelectListItems(SeasonList());
+            defaultModel.Origins = GetSelectListItems(new List<string> { "HongKong", "Vietnam" });
+            defaultModel.Ports = GetSelectListItems(new List<string> { "Port 1", "Port 2", "Port 3" });
+            defaultModel.Modes = GetSelectListItems(new List<string> { "Road", "Sea", "Air" });
+            
+            return View(defaultModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,OrderDate,Buyer,Currency,Season,Department,Vendor,Company,Origin,PortOfLoading,PortOfDelivery,OrderType,Factory,Mode,ShipDate,LatestShipDate,DeliveryDate,POQuantity,Status")] AddModel addModel)
+        public ActionResult Create([Bind(Include = "Id,OrderDate,Buyer,Currency,Season,Department,Vendor,Company,Origin,PortOfLoading,PortOfDelivery,OrderType,Factory,Mode,ShipDate,LatestShipDate,DeliveryDate,Status")] AddModel addModel)
         {
+            addModel.Seasons = GetSelectListItems(SeasonList());
+            addModel.Origins = GetSelectListItems(new List<string> { "HongKong", "Vietnam" });
+            addModel.Ports = GetSelectListItems(new List<string> { "Port 1", "Port 2", "Port 3" });
+            addModel.Modes = GetSelectListItems(new List<string> { "Road", "Sea", "Air" });
+
             if (ModelState.IsValid)
             {
-                db.AddModels.Add(addModel);
-                db.SaveChanges();
+                OrderModel order = new OrderModel();
+
+                order.Id = addModel.Id;
+                order.OrderDate = addModel.OrderDate;
+                order.Buyer = addModel.Buyer;
+                order.Currency = addModel.Currency;
+                order.Season = addModel.Season;
+                order.Department = addModel.Department;
+                order.Vendor = addModel.Vendor;
+                order.Company = addModel.Company;
+                order.Origin = addModel.Origin;
+                order.PortOfLoading = addModel.PortOfLoading;
+                order.PortOfDelivery = addModel.PortOfDelivery;
+                order.OrderType = addModel.OrderType;
+                order.Factory = addModel.Factory;
+                order.Mode = addModel.Mode;
+                order.ShipDate = addModel.ShipDate;
+                order.LatestShipDate = addModel.LatestShipDate;
+                order.DeliveryDate = addModel.DeliveryDate;
+                order.Status = addModel.Status;
+
+                order.POQuantity = 0;
+
+                order.Supplier = "";
+
+                PurchaseOrder.Add(order);
+
                 return RedirectToAction("Index");
             }
 
             return View(addModel);
-        }*/
+        }
+
+        private IEnumerable<string> SeasonList()
+        {
+            List<string> seasonList = new List<string>();
+            for (int i = 2010; i<=2020; i++)
+            {
+                seasonList.Add(i.ToString());
+            }
+            return seasonList;
+        }
+
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<string> elements)
+        {
+            var selectList = new List<SelectListItem>();
+
+            foreach (var element in elements)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+
+            return selectList;
+        }
 
         // GET: PurchaseOrder
         /*public ActionResult Index()
@@ -51,29 +124,6 @@ namespace InternProject.Controllers
             {
                 return HttpNotFound();
             }
-            return View(addModel);
-        }
-
-        // GET: PurchaseOrder/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: PurchaseOrder/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,OrderDate,Buyer,Currency,Season,Department,Vendor,Company,Origin,PortOfLoading,PortOfDelivery,OrderType,Factory,Mode,ShipDate,LatestShipDate,DeliveryDate,POQuantity,Status")] AddModel addModel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.AddModels.Add(addModel);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
             return View(addModel);
         }
 
