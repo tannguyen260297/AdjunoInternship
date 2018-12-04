c6f173101b151075426d2e89ea28887ffa79257c
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-04 16:15:32 +0700

    Separate DAL+BLL from Presentation Layer by adding DAL_BLL_Layer Project

---
+++ DAL_BLL_Layer/BLL/Interface/PurchaseOrderTool/IPurchaseOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_BLL_Layer.DAL.Models.OrderTrack;
+
+namespace DAL_BLL_Layer.BLL.Interface.PurchaseOrderTool
+{
+    public interface IPurchaseOrderRepository
+    {
+        void Add(OrderModel order);
+    }
+}
