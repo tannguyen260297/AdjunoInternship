7e832c464807518947b1d4a1f2c9233fb5de90b0
tannguyen2602 <tan.nguyen@groovetechnology.com>
2018-12-05 11:06:06 +0700

    update layout

---
+++ InternProject/Controllers/DisplayAndFilterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL_Layer.DAL.Models;
+
+namespace InternProject.Controllers
+{
+    public class DisplayAndFilterController : Controller
+    {
+        // GET: DisplayAndFilter
+        public ActionResult DisplayAndFilter()
+        {
+            return View();
+        }
+    }
+}
